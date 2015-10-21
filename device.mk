0e09b23fb844314eddd9a2752fb3ca4b51732704
maxwen <max.weninger@gmail.com>
2015-10-21 18:42:05 +0200

    find7: 6.0 bringup
    selinux disabled for the bringup
    
    Change-Id: I5207068cd2fbb3ae710027e5f1dc656597a50615

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Include common makefile
 $(call inherit-product, device/oppo/msm8974-common/common.mk)
 
+$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-hwui-memory.mk)
+
 LOCAL_PATH := device/oppo/find7
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -13,8 +16,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.find7.rc:root/init.qcom.rc \
-    $(LOCAL_PATH)/configs/init.fs.rc.std:root/init.fs.rc.std \
-    $(LOCAL_PATH)/configs/init.fs.rc.lvm:root/init.fs.rc.lvm \
     $(LOCAL_PATH)/configs/fstab.find7.std:root/fstab.qcom.std \
     $(LOCAL_PATH)/configs/fstab.find7.lvm:root/fstab.qcom.lvm \
     $(LOCAL_PATH)/configs/init.recovery.find7.rc:root/init.recovery.qcom.rc \
@@ -31,8 +32,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lvm/lvm.conf:root/lvm/etc/lvm.conf
 
 #sdcard permissions fix
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/init.sdcard_perms.sh:system/etc/init.sdcard_perms.sh
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/configs/init.sdcard_perms.sh:system/etc/init.sdcard_perms.sh
 
 # NFC packages
 PRODUCT_PACKAGES += \
