5659735556a9009e8a37b8563ef585722d8b1450
maxwen <max.weninger@gmail.com>
2015-10-21 18:42:05 +0200

    find7: 6.0 bringup
    Change-Id: I5207068cd2fbb3ae710027e5f1dc656597a50615

--- lvm/lvm_init.sh
+++ lvm/lvm_init.sh
@@ -10,9 +10,7 @@ export PATH=/sbin
 if [ -e /dev/lvpool/userdata ]; then
     /sbin/static/busybox cp /fstab.qcom.lvm /fstab.qcom
     /sbin/static/busybox cp /etc/twrp.fstab.lvm /etc/twrp.fstab
-    /sbin/static/busybox cp /init.fs.rc.lvm /init.fs.rc
 else
     /sbin/static/busybox cp /fstab.qcom.std /fstab.qcom
     /sbin/static/busybox cp /etc/twrp.fstab.std /etc/twrp.fstab
-    /sbin/static/busybox cp /init.fs.rc.std /init.fs.rc
 fi
