659a51718ecc6a74d1c8906d1a3fc973f0c28720
maxwen <max.weninger@gmail.com>
2015-12-18 00:06:01 +0100

    find7: DSPManager is back and new bootanimation
    Change-Id: I920d29d4e2ce3faf7f01df94f774b463268ab00c

--- omni_find7.mk
+++ omni_find7.mk
@@ -29,10 +29,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 720x480
-
-# no DSPManager for us
-TARGET_NO_DSPMANAGER := true
+TARGET_BOOTANIMATION_SIZE := 1080x608
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
