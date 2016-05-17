fc616702497905893460ee2512b6f0e2551b5f8e
maxwen <max.weninger@gmail.com>
2016-05-18 00:07:06 +0200

    WIP: TWRP and decrypt
    Change-Id: I252e03c60b25fdef6c70cf0061e0eabec970513c

--- device.mk
+++ device.mk
@@ -17,18 +17,20 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.find7.rc:root/init.qcom.rc \
     $(LOCAL_PATH)/configs/fstab.find7.std:root/fstab.qcom.std \
-    $(LOCAL_PATH)/configs/fstab.find7.lvm:root/fstab.qcom.lvm \
-    $(LOCAL_PATH)/configs/init.recovery.find7.rc:root/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/configs/twrp.fstab.lvm:recovery/root/etc/twrp.fstab.lvm \
-    $(LOCAL_PATH)/configs/twrp.fstab.std:recovery/root/etc/twrp.fstab.std
+    $(LOCAL_PATH)/configs/fstab.find7.lvm:root/fstab.qcom.lvm
+
+#    $(LOCAL_PATH)/configs/init.recovery.find7.rc:root/init.recovery.qcom.rc \
+#    $(LOCAL_PATH)/configs/twrp.fstab.lvm:recovery/root/etc/twrp.fstab.lvm \
+#    $(LOCAL_PATH)/configs/twrp.fstab.std:recovery/root/etc/twrp.fstab.std
 
 # LVM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lvm/lvm_init.sh:root/lvm_init.sh \
-    $(LOCAL_PATH)/lvm/lvm_init_recovery.sh:recovery/root/sbin/lvm_init_recovery.sh \
     $(LOCAL_PATH)/lvm/lvm_setprop.sh:root/lvm_setprop.sh \
     $(LOCAL_PATH)/lvm/lvm:root/sbin/lvm \
     $(LOCAL_PATH)/lvm/lvm.conf:root/lvm/etc/lvm.conf
+
+#    $(LOCAL_PATH)/lvm/lvm_init_recovery.sh:recovery/root/sbin/lvm_init_recovery.sh \
 
 # device specific packages
 PRODUCT_PACKAGES += \
