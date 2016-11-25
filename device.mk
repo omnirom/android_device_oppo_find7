4a6214b21b93adfb25337cc8170fc987164a73dc
maxwen <max.weninger@gmail.com>
2016-11-25 21:44:27 +0100

    find7: bringup for 7.1
    -update dependencies
    -fix for BT
    -fixes for lvm (we wont support anything else from now on to stop fighting against selinux more then needed)
    -update libinit for 7
    
    Change-Id: If580b1b8f0602fcc748d0b24f3952b9a4f9a081b

--- device.mk
+++ device.mk
@@ -6,8 +6,8 @@
 # Include common makefile
 $(call inherit-product, device/oppo/msm8974-common/common.mk)
 
-$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-dalvik-heap.mk)
-$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.mk)
 
 LOCAL_PATH := device/oppo/find7
 
@@ -16,25 +16,25 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/init.find7.rc:root/init.qcom.rc \
-    $(LOCAL_PATH)/configs/fstab.find7.std:root/fstab.qcom.std \
-    $(LOCAL_PATH)/configs/fstab.find7.lvm:root/fstab.qcom.lvm \
+    $(LOCAL_PATH)/configs/fstab.find7.lvm:root/fstab.qcom \
     $(LOCAL_PATH)/configs/init.recovery.find7.rc:root/init.recovery.qcom.rc \
     $(LOCAL_PATH)/configs/twrp.fstab.lvm:recovery/root/etc/twrp.fstab.lvm \
     $(LOCAL_PATH)/configs/twrp.fstab.std:recovery/root/etc/twrp.fstab.std
 
 # LVM
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/lvm/lvm_init.sh:root/lvm_init.sh \
     $(LOCAL_PATH)/lvm/lvm_init_recovery.sh:recovery/root/sbin/lvm_init_recovery.sh \
-    $(LOCAL_PATH)/lvm/lvm_setprop.sh:root/lvm_setprop.sh \
     $(LOCAL_PATH)/lvm/lvm:root/sbin/lvm \
     $(LOCAL_PATH)/lvm/lvm.conf:root/lvm/etc/lvm.conf
 
-# device specific packages
+# nfc
 PRODUCT_PACKAGES += \
     libnfc \
     libnfc_jni \
-    Nfc \
+    Nfc
+
+#cam 
+PRODUCT_PACKAGES += \
     SnapdragonCamera
 
 # Sensor configuration from Oppo
