775ed5511647d05918590e1ae475f584262d9d1d
maxwen <max.weninger@gmail.com>
2017-09-01 17:38:52 +0200

    find7: start with android-8.0
    Change-Id: Ibd16ddae46c2186d7a677cbb5a43254e556bf9ca

--- init/Android.mk
+++ init/Android.mk
@@ -4,7 +4,8 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := system/core/init \
+    system/core/base/include
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_find7.cpp
 
