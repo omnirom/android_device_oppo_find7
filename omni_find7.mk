3f829ccb2483259ac5b5d91141bad882f0952477
maxwen <max.weninger@gmail.com>
2017-10-08 23:43:07 +0200

    find7: fix the change to the new botanimation
    Change-Id: I6c7d3bd8382a524854b9a6ec50eb5758b17a31f9

--- omni_find7.mk
+++ omni_find7.mk
@@ -29,7 +29,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080x638
+TARGET_BOOTANIMATION_SIZE := 1080p
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
